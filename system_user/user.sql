bf948545e582e9aac4be344b9523371b5d3c08a4
{Tran Tuan Anh} <{trantuananh070405@gmail.com}>
2025-04-03 22:35:08 +0700

    upload lan 44

--- system_user/user.sql
+++ system_user/user.sql
@@ -1,8 +1,13 @@
-Create database system_;
+Create database system_user;
 use system_user;
 
 create table user(
 id int auto_increment primary key,
-username varchar(50) unique not null,
-password varchar(255) not null
+user varchar(50) unique not null,
+pass varchar(255) not null
 );
+
+insert into user(user, pass)
+values
+("admin", "123"),
+("nguoi dung", "456");
