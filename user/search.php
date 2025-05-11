b9535fc7956b39c2d2624dfac600eec6d69eff7d
Hai Dang <thd13062005@gmail.com>
2025-05-11 16:34:47 +0700

    Bỏ blog

--- user/search.php
+++ user/search.php
@@ -69,15 +69,6 @@
                 <img src="../asset/img/create.svg" alt="create" id="create" width="30px" height="30px" />
                 <span class="tooltip">Đăng tải</span>
             </a>
-
-            <!-- nút blog -->
-            <a href="#" class="navbar-button">
-                <svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" viewBox="0 0 512 512" id="blog">
-                    <path fill="#ffffff"
-                        d="M168 80c-13.3 0-24 10.7-24 24l0 304c0 8.4-1.4 16.5-4.1 24L440 432c13.3 0 24-10.7 24-24l0-304c0-13.3-10.7-24-24-24L168 80zM72 480c-39.8 0-72-32.2-72-72L0 112C0 98.7 10.7 88 24 88s24 10.7 24 24l0 296c0 13.3 10.7 24 24 24s24-10.7 24-24l0-304c0-39.8 32.2-72 72-72l272 0c39.8 0 72 32.2 72 72l0 304c0 39.8-32.2 72-72 72L72 480zM176 136c0-13.3 10.7-24 24-24l96 0c13.3 0 24 10.7 24 24l0 80c0 13.3-10.7 24-24 24l-96 0c-13.3 0-24-10.7-24-24l0-80zm200-24l32 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-32 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm0 80l32 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-32 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zM200 272l208 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-208 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm0 80l208 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-208 0c-13.3 0-24-10.7-24-24s10.7-24 24-24z" />
-                </svg>
-                <span class="tooltip">Blog</span>
-            </a>
             <!-- nút chuông thông báo -->
             <button class="navbar-button" id="notificationButton">
                 <svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" viewBox="0 0 448 512" id="bell">
@@ -101,12 +92,9 @@
             </button>
 
             <!-- Khung menu chủ đề -->
-            <div id="menuPanel" style="display: none;">
-                <button class="menu-item">THIÊN NHIÊN</button>
-                <button class="menu-item">ANIME</button>
-                <button class="menu-item">HÌNH NỀN MÁY TÍNH</button>
-                <button class="menu-item">ẢNH NGHỆ THUẬT</button>
-            </div>
+            <div id="menuPanel" style="display: none;"></div>
+            <!-- Nơi hiển thị ảnh -->
+            <div id="imageContainer" style="margin-top: 20px;"></div>
 
         </div>
         <!-- Khung thông báo -->
